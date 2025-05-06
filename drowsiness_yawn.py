cb95b95311c8585ffb23c70688b04299f75d529b
PranitaM246 <pranitam2469@gmail.com>
2025-05-06 14:45:51 +0530

    Update drowsiness_yawn.py

--- drowsiness_yawn.py
+++ drowsiness_yawn.py
@@ -52,10 +52,10 @@ def final_ear(shape):
     return (ear, leftEye, rightEye)
 
 def lip_distance(shape):
-    top_lip = shape[50:53]
+    top_lip = shape[40:53]
     top_lip = np.concatenate((top_lip, shape[61:64]))
 
-    low_lip = shape[56:59]
+    low_lip = shape[50:59]
     low_lip = np.concatenate((low_lip, shape[65:68]))
 
     top_mean = np.mean(top_lip, axis=0)
